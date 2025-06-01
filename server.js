ee6d36b01f52ba9f2d441bc627e5865a4a98bc45
Iris Contado <135505136+flinch2017@users.noreply.github.com>
2025-06-02 01:33:10 +0800

    .

--- server.js
+++ server.js
@@ -25,12 +25,6 @@ const pool = new Pool({
   },
 });
 
-pool.connect()
-  .then(client => {
-    console.log('✅ Connected to AWS RDS PostgreSQL');
-    client.release();
-  })
-  .catch(err => console.error('❌ Connection error', err.stack));
 
 
 // Middleware
