eed21267210bde822a176ef21cb027a1edee0bc9
Iris Contado <135505136+flinch2017@users.noreply.github.com>
2025-08-06 02:20:41 +0800

    .

--- routes/getSignUp.js
+++ routes/getSignUp.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-// GET /signup
-// GET /signup
+
 router.get('/signup', (req, res) => {
   res.render('signup', {
     error: '',
